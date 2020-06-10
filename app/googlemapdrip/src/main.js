caf1bf7038f8272fe269e1b4fcf9cf0b9c37f9a7
岡本恵太 <okamoto.keita1937@gmail.com>
2020-06-10 23:10:29 +0900

    GoogleMapAPIをインストール、GoogleMapのサンプル表示を実装

--- app/googlemapdrip/src/main.js
+++ app/googlemapdrip/src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/ja'
-import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/ja";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI, {locale})
+Vue.use(ElementUI, { locale });
 
 new Vue({
   router,
--- app/googlemapdrip/src/views/Home.vue
+++ app/googlemapdrip/src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    sssss
+    <MapSearch />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
-
+import MapSearch from "@/components/MapSearch.vue";
 export default {
   name: "Home",
   components: {
+    MapSearch
   }
 };
 </script>
