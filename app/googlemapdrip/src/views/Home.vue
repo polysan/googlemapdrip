795c98b41e5f257995bf35088e3d5e40905c1fda
岡本恵太 <okamoto.keita1937@gmail.com>
2020-06-21 00:14:52 +0900

    PlaceAPI用のコンポーネントを作成

--- app/googlemapdrip/src/views/Home.vue
+++ app/googlemapdrip/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <MapSearch />
+    <MapPlaces />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import MapSearch from "@/components/MapSearch.vue";
+import MapPlaces from "@/components/MapPlaces.vue";
 export default {
   name: "Home",
   components: {
-    MapSearch
+    MapSearch,
+    MapPlaces
   }
 };
 </script>
